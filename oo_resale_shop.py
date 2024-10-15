fe2d92c52a40cfb830d3ae4fd4416d46f4ac8306
Jenna Paczkowski <jpaczkowski@smith.edu>
2024-10-15 13:10:31 -0400

    Final Changes to code

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,45 +1,39 @@
 class ResaleShop:
  
-    inventory:list
+    inventory: list
 
-    def __init__(self):
-        self.inventory = []
-         
+    def __init__(self, inventory:list):
+        self.inventory = ["Computer #1"] 
 
-    #Sells a computer, or removes it from the store inventory 
-    def sell (self, c, inventory:list):
-        if (c in inventory): 
+
+    def sell (self, c):
+        if (c in self.inventory): 
             print("Trying to remove", c, "from inventory...")
-            inventory.remove(c)
+            self.inventory.remove(c)
             print("Success!")
         else: 
             print(f"{c} is not in the inventory!")
 
-    #Refurbishes the computer based on the year it's made 
-    def refurbish (self, c, new_os: str, inventory):
-        if (c in inventory):
-            print("Trying to refurbish item...") 
-        else:
-            print(f"{c} is not in the inventory!")
 
+    def refurbish(self, c, new_os: str):
+        print("Trying to refurbish item...")
 
-    #Buys a computer, or adds a computer to the inventory 
+        
     def buy(self, c):
         print("Trying to add", c, "to inventory...")
         self.inventory.append(c)
         print("Success!")
 
-    #Prints the inventory 
     def printinventory(inventory:list):
         print(inventory)
 
-#Defines a shop that using the ResaleShop class and simulates transactions 
+
 def main():
     myShop: ResaleShop = ResaleShop()
-    print("There are ", len(myShop.inventory), "item(s) in stock.")
-    c = "MY COMPUTER"
+    print("There are", len(myShop.inventory), "items in stock.")
+    c = "MY NEW COMPUTER"
     myShop.buy(c)
-    print("There are ", len(myShop.inventory), "item(s) in stock.")
+    print("There are now", len(myShop.inventory), "items in stock.")
     
 
 if __name__ == "__main__":
--- rubric.md
+++ rubric.md
@@ -17,34 +17,27 @@ __YES__ 1 pt: Constructor for `Computer` class is present and sets attributes co
 
 ### Level 2 (3 pts)
 
-___YES_ 1 pt: All expected attributes for `ResaleShop` class are present
-
+___YES_ 1 pt: All expected attributes for `ResaleShop` class are present=
 __YES__ 1 pt: All expected methods for `ResaleShop` class are present
 
-___PART__ 1 pt: Constructor for `ResaleShop` class is present and sets attributes correctly
-
+___YES_ 1 pt: Constructor for `ResaleShop` class is present and sets attributes correctly
 ### Level 3 (4 pts)
 
 __YES__ 1 pt: Can successfully add a new `Computer` to the inventory
 
 ___YES_ 1 pt: Can successfully remove a `Computer` from the inventory
-
 _YES___ 1 pt: Can successfully update the price/OS of a `Computer` contained in the inventory
-
-__PART__ 1 pt: Program prints appropriate error messages
+__YES__ 1 pt: Program prints appropriate error messages
 
 ## General Items (10 pts):
 
 __PART___ 4 pts: Program executes fully & consistently without crashing (exception/freeze)
-
 __PART___ 1 pt: Classes & class members all have header comments
+___PART__ 1 pt: Clear and consistent indentation
 
-__YES___ 1 pt: Clear and consistent indentation
+_____ 1 pt: Adheres to conventions on naming / capitalization
 
-__PART___ 1 pt: Adheres to conventions on naming / capitalization
-
-__PART___ 1 pt: Methods and variables all have clear and descriptive names
+_____ 1 pt: Methods and variables all have clear and descriptive names
 
 ___YES__ 1 pt: All required files included with submission (including completed checklist file)
-
 ___YES__ 1 pt: `reflection.md` contains your reflection on the assignment
